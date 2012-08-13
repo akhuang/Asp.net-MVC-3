d1b5e14bc78efba1a2941e5653edbbba0a073db9
phoenix <hf888.huang@gmail.com>
2012-08-12 20:34:52 -0700

    Add SportStore

---
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public int PageSize = 4;
+
+        private IProductRepository productRep;
+        public ProductController(IProductRepository productRepParam)
+        {
+            productRep = productRepParam;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            //return View(productRep.Products
+            //        .OrderBy(p => p.ProductID)
+            //        .Skip((page - 1) * PageSize)
+            //        .Take(PageSize)
+            //    );
+
+            ProductsListViewModel viewModel = new ProductsListViewModel
+            {
+                Products = productRep.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = productRep.Products.Count()
+                }
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Routing;
+using System.Web.Mvc;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product{Name ="Football",Price=25},
+                new Product{Name ="Surf board",Price=179},
+                new Product{Name ="Running shoes",Price=95}
+            }.AsQueryable());
+
+            //ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+
+            ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
