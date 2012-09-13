55b9d30ce1c2d1676a440274ae05c5415fff2333
unknown <phoenix_huang@phoenix.keyou.corp>
2012-09-13 16:33:10 +0800

    Init

---
+++ Autofac_Demo/AutofacDemo.Core/Logging/OrchardLog4netFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using Castle.Core.Logging;
+using log4net;
+using log4net.Config;
+
+namespace Orchard.Logging
+{
+    public class OrchardLog4netFactory : AbstractLoggerFactory
+    {
+        private static bool _isFileWatched = false;
+        
+        public OrchardLog4netFactory()
+            : this(ConfigurationManager.AppSettings["log4net.Config"]) { }
+
+        public OrchardLog4netFactory(string configFilename)
+        {
+            if (!_isFileWatched && !string.IsNullOrWhiteSpace(configFilename))
+            {
+                // Only monitor configuration file in full trust
+                XmlConfigurator.ConfigureAndWatch(GetConfigFile(configFilename));
+                _isFileWatched = true;
+            }
+        }
+
+        public override Castle.Core.Logging.ILogger Create(string name, LoggerLevel level)
+        {
+            throw new NotSupportedException("Logger levels cannot be set at runtime. Please review your configuration file.");
+        }
+
+        public override Castle.Core.Logging.ILogger Create(string name)
+        {
+            return new OrchardLog4netLogger(LogManager.GetLogger(name), this);
+        }
+    }
+}
