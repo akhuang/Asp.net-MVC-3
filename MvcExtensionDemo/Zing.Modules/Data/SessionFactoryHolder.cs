7527057f6cbdf83f99e9c51da344cf85990fa8f6
phoenix <fy24h.huang@gmail.com>
2013-03-01 17:03:56 +0800

    add FluentNHibernate

---
+++ MvcExtensionDemo/Zing.Modules/Data/SessionFactoryHolder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+
+namespace Zing.Modules.Data
+{
+    public class SessionFactoryHolder
+    {
+        private static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+              .Database(MsSqlConfiguration.MsSql2008.ConnectionString(c => c.FromConnectionStringWithKey("DefaultConnection")))
+              .BuildSessionFactory();
+        }
+    }
+}
